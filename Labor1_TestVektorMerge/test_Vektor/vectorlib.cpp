8b72470f4bd34156193cc89e237842e0f8d605be
PhilippHubr <ph-hu@web.de>
2021-03-24 16:51:24 +0100

    EinlesenDerVektore

--- Labor1_TestVektorMerge/test_Vektor/vectorlib.cpp
+++ Labor1_TestVektorMerge/test_Vektor/vectorlib.cpp
@@ -1,5 +1,41 @@
+//Informatik 2 Labor – 3er Team(Abgesprochen mit Herr Prof.Wietzke) 
+// 
+//Teilnehmer			Matr.Nr.	Kurs			Semester	VS- Version		Betriebssystem
+//Philipp Huber			63326	Maschinenbau(Mabb)	6			2019 Community	Windows 10
+//Marius Grumer			63284	Maschinenbau(Mabb)	6			2019 Community	Windows 10
+//Sebastian Schmitt		63304	Maschinenbau(Mabb)	6			2019 Community	Windows 10
+//
+//Datum: 2021.03.24
+//
+//Sinn, Zweck der Datei:
+//Implementieren der Funktionen
+//
+
+
 #include"vectorlib.h"
 using namespace std;
+
+void einlesenLaenge(int* vLen, int Vektornummer)
+{
+	do
+	{
+		cout << "Geben Sie die Laenge des "<<Vektornummer<<". Vectors ein (max. 10) -> ";
+		cin >> *vLen;															//Wert für vLen einlesen
+	} while (*vLen > 10 || *vLen < 1);											// Fehlerabfrage, ob die Länge zwischen der Erlaubten 1 und 10 als Länge liegt
+																				//Schleife läuft so lange bis vLen fpr das richtige Intervall angegeben wurde
+}
+
+void einlesen(int* vektoradr, int vektLen)					
+{
+	int n = 0;																	//Laufindex um die n.te postion des Vektors anzugeben
+	for (int* ptr = vektoradr; (ptr - vektoradr) < vektLen; ptr++)				//Schleife, die fpr jede Position einen Wert für den Vektor einliest
+	{
+		cout << n + 1 << ". Vektorelement -> ";
+		cin >> *ptr;
+		n++;
+	}
+
+}
 
 
 int merge(int* v1, int v1Len, int* v2, int v2Len, int* vDest) 
@@ -46,12 +82,12 @@ int merge(int* v1, int v1Len, int* v2, int v2Len, int* vDest)
 }
 
 
-bool isSorted(int* v, int vLen)									//Die Funktion isSorted überprüft ob die Vektoren aufsteigend sortiert sind, gibt true oder false zurück
+bool isSorted(int* v, int vLen)											//Die Funktion isSorted überprüft ob die Vektoren aufsteigend sortiert sind, gibt true oder false zurück
 {
-	vLen--;															//vLen muss um 1 reduziert werden, da  nicht überprüft werden muss ob der Letzte Wert kleiner als ein darauf folgender Wert ist.
-	for (int* i = v; (i - v) < vLen; i++)							//Schleife zum überprüfen von jedem Wert
+	vLen--;																//vLen muss um 1 reduziert werden, da  nicht überprüft werden muss ob der Letzte Wert kleiner als ein darauf folgender Wert ist.
+	for (int* i = v; (i - v) < vLen; i++)								//Schleife zum überprüfen von jedem Wert
 	{
-		if (*i > *(i + 1))											//Bedingung erfüllt falls ein Wert größer als sein darauf folgender Wert ist
+		if (*i > *(i + 1))												//Bedingung erfüllt falls ein Wert größer als sein darauf folgender Wert ist
 		{
 			//cout << *i << " ist groesser als " << *(i + 1) << endl;	//optionale Fehlermeldung 
 			return false;
@@ -61,7 +97,7 @@ bool isSorted(int* v, int vLen)									//Die Funktion isSorted überprüft ob die
 }
 
 
-void ausgabe(int* v, int vLen)		//Ausgabe eines Vektors in {...}
+void ausgabe(int* v, int vLen)											//Ausgabe eines Vektors in {...}
 {
 	cout << "{ ";
 	for (int* ptr = v; (ptr - v) < vLen; ptr++)
--- Labor1_TestVektorMerge/test_Vektor/vectorlib.h
+++ Labor1_TestVektorMerge/test_Vektor/vectorlib.h
@@ -1,8 +1,27 @@
+//Informatik 2 Labor – 3er Team(Abgesprochen mit Herr Prof.Wietzke)
+// 
+//Teilnehmer			Matr.Nr.	Kurs			Semester	VS- Version		Betriebssystem
+//Philipp Huber			63326	Maschinenbau(Mabb)	6			2019 Community	Windows 10
+//Marius Grumer			63284	Maschinenbau(Mabb)	6			2019 Community	Windows 10
+//Sebastian Schmitt		63304	Maschinenbau(Mabb)	6			2019 Community	Windows 10
+//
+//Datum: 2021.03.23
+//
+//Sinn, Zweck der Datei:
+//Deklarieren der Funktionen
+//
+
+
+
 #pragma once
 #include <iostream>
 using namespace std;
 
-int merge(int* v1, int v1Len, int* v2, int v2Len, int* vDest);			//Wie in Aufgabenstellung gefordert
+void einlesenLaenge(int* vLen, int Vektornummer);											//Einlesen der Länge des Vektors (bsp 1. Vektor hat die Vektornummer = 1)
+
+void einlesen(int* vektoradr, int vektLen);													//Funktion zum Einlesen von Vektoren
+
+int merge(int* v1, int v1Len, int* v2, int v2Len, int* vDest);								//Wie in Aufgabenstellung gefordert
 
 bool isSorted(int* v, int vLen);
 
